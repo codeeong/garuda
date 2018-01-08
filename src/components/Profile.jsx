c1c159754de0f50946ee2acd5b22351efe96a721
Codee <codee@codee.local>
2018-01-08 14:58:06 +0800

    fifth commit

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import Slider from 'material-ui/Slider';
+import TextField from 'material-ui/TextField';
+import Checkbox from 'material-ui/Checkbox';
 
 const styles = {
   headline: {
@@ -8,8 +10,13 @@ const styles = {
     paddingTop: 16,
     marginBottom: 12,
     fontWeight: 400,
+    color: '#ffffff'
+  },
+  checkbox: {
+    marginBottom: 16,
   },
 };
+
 
   var cardStyle = {
     display: 'block',
@@ -17,38 +24,93 @@ const styles = {
     transitionDuration: '0.3s',
     height: '50%',
     marginLeft: '10%',
-    marginTop: '5%'
+    marginTop: '5%',
+    backgroundColor: '#383838',
 };
 
-
-function handleActive(tab) {
-  alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
+var textStyle = {
+  marginLeft: '3%',
+  marginRight: '3%'
 }
 
+var textColor = {
+  color: '#d3d3d3'
+}
+
+
 const TabsExampleSimple = () => (
-  <Tabs style = {cardStyle}>
-    <Tab label="Account" >
-      <div>
+  <Tabs style ={cardStyle}>
+    <Tab label="Account">
+      <div style ={textStyle}>
         <h2 style={styles.headline}>Create New Account</h2>
-        <p>
-          This is an example tab.
+        <p style = {textColor}>
+          Email:
         </p>
-        <p>
-          You can put any sort of HTML or react component in here. It even keeps the component state!
+        <TextField
+          hintText="Email"
+        /><br />
+        <p style = {textColor}>
+          Password:
         </p>
-        <Slider name="slider0" defaultValue={0.5} />
+        <TextField
+          hintText="Password"
+        /><br />
+        <p style = {textColor}>
+          Confirm Password:
+        </p>
+        <TextField
+          hintText="Re-Enter Password"
+        /><br />
       </div>
     </Tab>
     <Tab label="Basic" >
-      <div>
+      <div style ={textStyle}>
         <h2 style={styles.headline}>Basic Details</h2>
-        <p>
-          This is another example tab.
+        <p style = {textColor}>
+          Company Name:
         </p>
+        <TextField
+          hintText="Company Name"
+        /><br />
+        <p style = {textColor}>
+          License Number:
+        </p>
+        <TextField
+          hintText="License Number"
+        /><br />
+        <p style = {textColor}>
+          Services Provided:
+        </p>
+        <Checkbox
+          label="Media Production"
+          style={styles.checkbox}
+        />
+        <Checkbox
+          label="Mapping"
+          style={styles.checkbox}
+        />
+        <Checkbox
+          label="Inspection"
+          style={styles.checkbox}
+        />
+        <Checkbox
+          label="Others"
+          style={styles.checkbox}
+        />
+        <TextField
+          hintText="Input Other Services Here"
+        /><br />
+
+        <p style = {textColor}>
+          About Us:
+        </p>
+        <TextField
+          hintText="Name"
+        /><br />
       </div>
     </Tab>
     <Tab label="Portfolio" >
-      <div>
+      <div style ={textStyle}>
         <h2 style={styles.headline}>Past Projects</h2>
         <p>
           Upload Images Below
@@ -56,7 +118,7 @@ const TabsExampleSimple = () => (
       </div>
     </Tab>
     <Tab label="Admin" >
-      <div>
+      <div style ={textStyle}>
         <h2 style={styles.headline}>Bank Details</h2>
         <p>
           This is another example tab.
@@ -64,14 +126,14 @@ const TabsExampleSimple = () => (
       </div>
     </Tab>
     <Tab label="T&Cs" >
-      <div>
+      <div style ={textStyle}>
         <h2 style={styles.headline}>Terms and Conditions</h2>
         <p>
           This is another example tab.
         </p>
       </div>
     </Tab>
-
+    
   </Tabs>
 );
 
